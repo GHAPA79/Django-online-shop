056fbd7e337cf21ee7959654050ae5e3ba7ab306
saman <samanghapanuri@gmail.com>
2023-08-02 16:15:00 +0330

    create Comment model

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', 'datetime_modified', ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'stars', 'active', 'datetime_modified', ]

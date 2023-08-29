57b22ae40999c3ba439967db59808e23f71ac182
saman <samanghapanuri@gmail.com>
2023-08-29 15:04:20 +0330

    add OrderForm and some change

--- orders/models.py
+++ orders/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.conf import settings
+from django.utils.translation import gettext as _
 
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    is_paid = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_('User'))
+    is_paid = models.BooleanField(_('Is paid?'), default=False)
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    address = models.CharField(max_length=700)
+    first_name = models.CharField(_('First name'), max_length=100)
+    last_name = models.CharField(_('Last name'), max_length=100)
+    phone_number = models.CharField(_('Phone number'), max_length=15)
+    address = models.CharField(_('Address'), max_length=700)
 
-    order_notes = models.CharField(max_length=800, blank=True)
+    order_notes = models.CharField(_('Order notes'), max_length=800, blank=True)
 
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    datetime_created = models.DateTimeField(_('Date time created'), auto_now_add=True)
+    datetime_modified = models.DateTimeField(_('date time modified'), auto_now=True)
 
     def __str__(self):
         return f'Order: {self.id}'
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .forms import OrderForm
+
 
 def create_order_view(request):
-    return render(request, 'orders/order_create.html')
+    return render(request, 'orders/order_create.html', context={
+        'form': OrderForm(),
+    })

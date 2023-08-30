724f837a9e820193468a8d2131b6cd0a071ae790
saman <samanghapanuri@gmail.com>
2023-08-30 15:18:16 +0330

    * Update order view for place an order section *

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 from .forms import OrderForm
+from .models import OrderItem
+from cart.cart import Cart
 
 
 @login_required
 def create_order_view(request):
     order_form = OrderForm()
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        messages.warning(request, _('You can not proceed to checkout page because your cart is empty'))
+        return redirect('products_list')
 
     if request.method == "POST":
         order_form = OrderForm(request.POST)
@@ -15,6 +24,21 @@ def create_order_view(request):
             order_obj = order_form.save(commit=False)
             order_obj.user = request.user
             order_obj.save()
+
+            for item in cart:
+                product = item['product_obj']
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=product,
+                    quantity=item['quantity'],
+                    price=product.price,
+                )
+
+            cart.clear()
+
+            request.user.first_name = order_obj.first_name
+            request.user.last_name = order_obj.last_name
+            request.user.save()
 
     return render(request, 'orders/order_create.html', context={
         'form': order_form,
